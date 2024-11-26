7116a33f6f2f041b9c2597d83f545a6449ed2e9d
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-11-26 16:20:28 +0300

    feat: add webhook endpoint

---
+++ WebhookRESTAPI/WebhookRESTAPI/Features/Webhooks/Endpoints/WebhookEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using WebhookRESTAPI.Data;
+
+namespace WebhookRESTAPI.Features.Webhooks.Endpoints
+{
+    public static class WebhookEndpoints
+    {
+        public static void MapWebhookEndpoints(this WebApplication app)
+        {
+            string groupName = "Webhooks";
+            var group = app.MapGroup("api/webhooks");
+
+            group.MapPost("/", (
+                [FromServices] ApplicationDbContext dbContext,
+                HttpRequest request,
+                CancellationToken cancellationToken) =>
+            {
+                return Results.Ok();
+            })
+            .WithTags(groupName)
+            .WithDescription("Creates a webhook event")
+            .WithSummary("Create a webhook event")
+            .Produces(StatusCodes.Status200OK)
+            .ProducesValidationProblem()
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status401Unauthorized)
+            .Produces(StatusCodes.Status403Forbidden)
+            .Produces(StatusCodes.Status500InternalServerError)
+            .RequireAuthorization();
+        }
+    }
+}
--- WebhookRESTAPI/WebhookRESTAPI/Program.cs
+++ WebhookRESTAPI/WebhookRESTAPI/Program.cs
@@ -1,6 +1,7 @@
 
 using WebhookRESTAPI.Core;
 using WebhookRESTAPI.Data;
+using WebhookRESTAPI.Features.Webhooks.Endpoints;
 
 namespace WebhookRESTAPI
 {
@@ -32,24 +33,7 @@ namespace WebhookRESTAPI
             
             SeedData.Initialize(app);
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+            app.MapWebhookEndpoints();
 
             app.Run();
         }
