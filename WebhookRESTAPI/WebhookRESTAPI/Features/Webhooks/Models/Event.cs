d15657858722ea0791be57633803988b1772c305
Ssewannonda Keith Edwin <skeith696@gmail.com>
2024-11-26 12:23:47 +0300

    feat: add models

---
+++ WebhookRESTAPI/WebhookRESTAPI/Features/Webhooks/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace WebhookRESTAPI.Features.Webhooks.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        
+        public DateTimeOffset Timestamp { get; set; }
+        
+        public string EventType { get; set; }
+        
+        public string Payload { get; set; }
+    }
+}
---
+++ WebhookRESTAPI/WebhookRESTAPI/Features/Webhooks/Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿namespace WebhookRESTAPI.Features.Webhooks.Models
+{
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+        
+        public Guid SubscriberId { get; set; }
+        
+        public EventType EventType { get; set; }
+
+        public string CallbackUrl { get; set; } = default!;
+        
+        public string? Secret { get; set; }
+    }
+}
